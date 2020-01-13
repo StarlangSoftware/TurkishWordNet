47294ead985a54b43676ee49ff3d71cd2ceb310d
Gökhan Ercan <gokhan@ercannet.com>
2020-01-13 03:05:58 +0300

    Fixed WuPalmer algorithm. Basic path operations implemented. Added autoSimulateRoot feature to safely handle hierarchical inconsistencies.

--- src/main/java/WordNet/Similarity/WuPalmer.java
+++ src/main/java/WordNet/Similarity/WuPalmer.java
@@ -3,6 +3,7 @@ package WordNet.Similarity;
 import WordNet.SynSet;
 import WordNet.WordNet;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
 
 public class WuPalmer extends Similarity{
@@ -12,9 +13,16 @@ public class WuPalmer extends Similarity{
     }
 
     public double computeSimilarity(SynSet synSet1, SynSet synSet2) {
-        ArrayList<String> pathToRootOfSynSet1 = wordNet.findPathToRoot(synSet1);
-        ArrayList<String> pathToRootOfSynSet2 = wordNet.findPathToRoot(synSet2);
-        float LCSdepth = wordNet.findLCSdepth(pathToRootOfSynSet1, pathToRootOfSynSet2);
-        return 2 * LCSdepth / (pathToRootOfSynSet1.size() + pathToRootOfSynSet2.size());
+        ArrayList<String> path1 = wordNet.findPathToRoot(synSet1);
+        ArrayList<String> path2 = wordNet.findPathToRoot(synSet2);
+        AbstractMap.SimpleEntry<String, Integer> lcs = findLCS(path1,path2,true);
+        float lcsDepth = lcs.getValue();
+        if(lcsDepth == -1) return -1;        //TODO: -1 is used for null returns. Should return nullable results
+        float c1len = findLength(path1,lcs);
+        float c2len = findLength(path2,lcs);
+
+        float num = 2 * lcsDepth;
+        float denom = c1len + c2len + num;
+        return num / denom;
     }
 }
