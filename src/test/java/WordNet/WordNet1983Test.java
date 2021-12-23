c3f58351dc04bd3523b7b6c1bb5409b78423269a
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-12-23 11:24:28 +0300

    Added 1983 dictionary. Updated 1983 wordnet.

--- src/test/java/WordNet/WordNet1983Test.java
+++ src/test/java/WordNet/WordNet1983Test.java
@@ -1,15 +1,114 @@
 package WordNet;
 
+import Dictionary.Pos;
+import Dictionary.TurkishWordComparator;
+import Dictionary.TxtDictionary;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Locale;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class WordNet1983Test extends PreviousWordNetTest{
 
     @Before
     public void setUp() {
         previuosWordNet = new WordNet("turkish1983_wordnet.xml", new Locale("tr"));
+    }
+
+    @Test
+    public void testExample() {
+        previousDictionary = new TxtDictionary("turkish1983_dictionary.txt", new TurkishWordComparator());
+        super.testExample();
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals(55262, previuosWordNet.size());
+    }
+
+    @Test
+    public void generateDictionary() {
+        generateDictionary("1983");
+    }
+
+    @Test
+    public void testSynSetList() {
+        int literalCount = 0;
+        for (SynSet synSet : previuosWordNet.synSetList()){
+            literalCount += synSet.getSynonym().literalSize();
+        }
+        assertEquals(72446, literalCount);
+    }
+
+    @Test
+    public void testGetSynSetsWithPartOfSpeech() {
+        assertEquals(28869, previuosWordNet.getSynSetsWithPartOfSpeech(Pos.NOUN).size());
+        assertEquals(13531, previuosWordNet.getSynSetsWithPartOfSpeech(Pos.VERB).size());
+        assertEquals(9197, previuosWordNet.getSynSetsWithPartOfSpeech(Pos.ADJECTIVE).size());
+        assertEquals(1860, previuosWordNet.getSynSetsWithPartOfSpeech(Pos.ADVERB).size());
+        assertEquals(1598, previuosWordNet.getSynSetsWithPartOfSpeech(Pos.INTERJECTION).size());
+        assertEquals(81, previuosWordNet.getSynSetsWithPartOfSpeech(Pos.CONJUNCTION).size());
+        assertEquals(66, previuosWordNet.getSynSetsWithPartOfSpeech(Pos.PRONOUN).size());
+        assertEquals(60, previuosWordNet.getSynSetsWithPartOfSpeech(Pos.PREPOSITION).size());
+    }
+
+    @Test
+    public void testLiteralSortedList() {
+        assertTrue(previuosWordNet.literalCorrectOrderCheck());
+    }
+
+    @Test
+    public void testLiteralList() {
+        assertEquals(51673, previuosWordNet.literalList().size());
+    }
+
+    @Test
+    public void testSameLiteralSameSenseCheck() {
+        for (Literal literal : previuosWordNet.sameLiteralSameSenseCheck()){
+            System.out.println(literal.getName());
+        }
+        assertEquals(0, previuosWordNet.sameLiteralSameSenseCheck().size());
+    }
+
+    @Test
+    public void testNoPosCheck() {
+        assertEquals(0, previuosWordNet.noPosCheck().size());
+    }
+
+    @Test
+    public void testNoDefinitionCheck() {
+        assertEquals(0, previuosWordNet.noDefinitionCheck().size());
+    }
+
+    @Test
+    public void testSameLiteralSameSynSetCheck() {
+        assertEquals(0, previuosWordNet.sameLiteralSameSynSetCheck().size());
+    }
+
+    @Test
+    public void testExistenceOfKeNetSynSets(){
+        currentWordNet = new WordNet();
+        super.testExistenceOfKeNetSynSets();
+    }
+
+    @Test
+    public void testExistenceOfPreviousSynSets(){
+        super.testExistenceOfPreviousSynSets("1944", "TUR01");
+        super.testExistenceOfPreviousSynSets("1955", "TUR02");
+        super.testExistenceOfPreviousSynSets("1959", "TUR03");
+        super.testExistenceOfPreviousSynSets("1966", "TUR04");
+        super.testExistenceOfPreviousSynSets("1969", "TUR05");
+        super.testExistenceOfPreviousSynSets("1974", "TUR06");
+    }
+
+    @Test
+    public void comparePosWithPosOfCorrespondingKeNetSynSets(){
+        currentWordNet = new WordNet();
+        super.comparePosWithPosOfCorrespondingKeNetSynSets();
     }
 
     public void findMatchingLiteralsInPreviousWordNets(){
