7189f10d9dae2aa459901c0c69c5b7b60933f4c8
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-06-11 11:44:49 +0300

    Added some U.S. cities.

--- src/test/java/WordNet/WordNetTest.java
+++ src/test/java/WordNet/WordNetTest.java
@@ -27,7 +27,7 @@ public class WordNetTest {
 
     @Test
     public void testSize() {
-        assertEquals(78311, turkish.size());
+        assertEquals(78324, turkish.size());
     }
 
     @Test
@@ -36,7 +36,7 @@ public class WordNetTest {
         for (SynSet synSet : turkish.synSetList()){
             literalCount += synSet.getSynonym().literalSize();
         }
-        assertEquals(110236, literalCount);
+        assertEquals(110254, literalCount);
     }
 
     @Test
@@ -46,7 +46,7 @@ public class WordNetTest {
 
     @Test
     public void testLiteralList() {
-        assertEquals(82135, turkish.literalList().size());
+        assertEquals(82151, turkish.literalList().size());
     }
 
     public void generateDictionary(){
@@ -164,9 +164,9 @@ public class WordNetTest {
             }
             counts.put(count);
         }
-        assertEquals(48704, (int) counts.get(1));
-        assertEquals(28417, (int) counts.get(2));
-        assertEquals(3556, (int) counts.get(3));
+        assertEquals(48714, (int) counts.get(1));
+        assertEquals(28421, (int) counts.get(2));
+        assertEquals(3558, (int) counts.get(3));
         assertEquals(910, (int) counts.get(4));
         assertEquals(304, (int) counts.get(5));
         assertEquals(137, (int) counts.get(6));
@@ -242,7 +242,7 @@ public class WordNetTest {
 
     @Test
     public void testGetSynSetsWithPartOfSpeech() {
-        assertEquals(43869, turkish.getSynSetsWithPartOfSpeech(Pos.NOUN).size());
+        assertEquals(43882, turkish.getSynSetsWithPartOfSpeech(Pos.NOUN).size());
         assertEquals(17772, turkish.getSynSetsWithPartOfSpeech(Pos.VERB).size());
         assertEquals(12410, turkish.getSynSetsWithPartOfSpeech(Pos.ADJECTIVE).size());
         assertEquals(2549, turkish.getSynSetsWithPartOfSpeech(Pos.ADVERB).size());
@@ -318,7 +318,7 @@ public class WordNetTest {
                 count++;
             }
         }
-        assertEquals(10987, count);
+        assertEquals(11000, count);
     }
 
     @Test
